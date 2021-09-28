f48c6d4707ddf0b22101ba3cdabfffc39faa47dc
james-work-account <31282758+james-work-account@users.noreply.github.com>
2021-09-28 09:54:22 +0100

    visual tweaks, min values

--- src/main.js
+++ src/main.js
@@ -5,19 +5,21 @@ import App from "./App.vue";
 const store = createStore({
   state() {
     return {
-      billAmount: 142.55,
-      tipPercentage: 0.15,
-      numberOfPeople: 5,
+      billAmount: null,
+      tipPercentage: null,
+      numberOfPeople: null,
     };
   },
   getters: {
     tipAmountPerPerson(state) {
-      if (!state.numberOfPeople) return Infinity;
+      if (state.numberOfPeople === 0) return Infinity;
+      if (!state.numberOfPeople) return 0;
       if (!state.tipPercentage) return state.billAmount;
       return (state.billAmount * state.tipPercentage) / state.numberOfPeople;
     },
     totalAmount(state, getters) {
-      if (!state.numberOfPeople) return Infinity;
+      if (state.numberOfPeople === 0) return Infinity;
+      if (!state.numberOfPeople) return 0;
       if (!state.tipPercentage) return state.billAmount;
       return (
         state.billAmount / state.numberOfPeople + getters.tipAmountPerPerson
@@ -25,9 +27,9 @@ const store = createStore({
     },
     everythingIsZero(state) {
       return (
-        state.billAmount === 0 &&
-        state.tipPercentage === 0 &&
-        state.numberOfPeople === 0
+        state.billAmount === null &&
+        state.tipPercentage === null &&
+        state.numberOfPeople === null
       );
     },
   },
@@ -44,9 +46,9 @@ const store = createStore({
   },
   actions: {
     resetAllValues({ commit }) {
-      commit("updateBillAmount", 0);
-      commit("updateTipPercentage", 0);
-      commit("updateNumberOfPeople", 0);
+      commit("updateBillAmount", null);
+      commit("updateTipPercentage", null);
+      commit("updateNumberOfPeople", null);
     },
   },
   devtools: true,
